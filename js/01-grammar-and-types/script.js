b8cd975afa959d5b97ce50f35123019bb4f11b14
Josh Birch <joshuabirch@protonmail.com>
2026-04-08 07:11:08 +0100

    Add references on JS from MDN docs

---
+++ js/01-grammar-and-types/script.js
@@ -0,0 +1,19 @@
+/* https://developer.mozilla.org/en-US/docs/Web/JavaScript */
+
+// Declares a variable, optionally initialising it to a value
+var x;
+
+// Declares a block-scoped, local variable, optionally initialising it to a value
+let y;
+
+// Declares a block-scoped, read-only named constant. `const` always needs an initialiser
+const z = 42;
+
+// print
+let greeting = "Hello, World!";
+console.log(greeting);
+
+// undefined
+console.log(x)
+
+

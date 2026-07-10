90f41efe6c6979490b8d9fa8a9bf761fad5e9be5
Josh Birch <joshuabirch@protonmail.com>
2026-07-10 18:06:28 +0100

    notes on pentesting; active recon

--- js/01-grammar-and-types/script.js
+++ js/01-grammar-and-types/script.js
@@ -1,12 +1,12 @@
 /* https://developer.mozilla.org/en-US/docs/Web/JavaScript */
 
-// Declares a variable, optionally initialising it to a value
+// declares a variable, optionally initialising it to a value
 var x;
 
-// Declares a block-scoped, local variable, optionally initialising it to a value
+// declares a block-scoped, local variable, optionally initialising it to a value
 let y;
 
-// Declares a block-scoped, read-only named constant. `const` always needs an initialiser
+// declares a block-scoped, read-only named constant. `const` always needs an initialiser
 const z = 42;
 
 // print

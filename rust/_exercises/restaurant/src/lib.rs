113a850c3bfc7d0bc7a84b7e2b27d321e6af7355
Josh Birch <joshuabirch@protonmail.com>
2026-07-24 11:42:51 +0100

    rust: references on packages

--- rust/_exercises/restaurant/src/lib.rs
+++ rust/_exercises/restaurant/src/lib.rs
@@ -8,22 +8,27 @@ pub fn eat_at_restaurant() {
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change bread
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    // meal.seasonal_fruit = String::from("blueberries")
+    // ^ won't compile
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn deliver_order() {
+    todo!();
 }
 
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {
+    // Making a module public doesn't make it's contents public
+    pub mod hosting {
+        pub fn add_to_waitlist() {
             todo!();
         }
         fn seat_at_table() {
@@ -43,5 +48,48 @@ mod front_of_house {
         fn take_payment() {
             todo!();
         }
+    }
+}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {
+        todo!();
+    }
+
+    // Structs can be public but fields stay private unless specified
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    // all variants of a public enum are public
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
     }
 }
